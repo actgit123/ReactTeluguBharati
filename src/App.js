7938c1f7613a15ed932bfe1a00df5307653c840c
actgit123 <pavaniv@outlook.com>
2020-01-22 12:26:56 -0800

    Footer

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/HeaderComponent';
+import Footer from './Components/FooterComponent';
 
 function App() {
   return (
-    <Header /> 
+    
+   
+    <Footer />
+    
    
   );
 }
---
+++ src/Components/FooterComponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import { Link } from 'react-router-dom';
+
+
+function Footer(){
+    return (
+
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col">©తెలుగు భారతి.</div>
+ <div class="col offset-md-7"><a href="contact.html">contact |</a><a href="privacypolicy">Privacy Policy</a></div></div>
+                <div class="row">
+                    <div class="col offset-md-1">
+                            <p> Telugu Bharati is a fully volunteer-run 501(c)(3) non-profit organization
+           registered in the state of Washington with Tax Id: 46-2431612</p>
+                        </div>
+                    </div>
+                    </div>
+                    
+                </footer>
+    );
+    }
+        export default Footer;
